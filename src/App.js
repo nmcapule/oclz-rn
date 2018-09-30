496254e8e33c1a1e300a2ff6fcbcef5bcbb43128
Nathaniel Capule <ncapule@ncapule.mnl.corp.google.com>
2018-09-30 17:08:16 +0800

    Setup thingie

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View style={styles.box}>
+        <Text style={styles.text}>Hello, world!</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  box: { padding: 10 },
+  text: { fontWeight: 'bold' }
+});
 
 export default App;
