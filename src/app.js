08ca2a49d1f6126530ea54f83476302399ab9753
Nathaniel Capule <ncapule@ncapule.mnl.corp.google.com>
2018-09-30 23:02:04 +0800

    we encrypt now boiz

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import * as nacl from 'tweetnacl-util';
+import * as sha256 from 'fast-sha256';
+
 function loadMovies(cb) {
   fetch('https://facebook.github.io/react-native/movies.json')
       .then(response => response.json())
@@ -11,20 +14,25 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ohshit:'',
+      data:'',
     };
   }
 
   componentDidMount() {
-    loadMovies(json => this.setState({
-      ohshit: JSON.stringify(json),
-    }));
+    loadMovies(json => {
+      const strjson = JSON.stringify(json);
+      const utf8in = nacl.decodeUTF8(strjson);
+      const utf8out = sha256.hmac('mahkey', utf8in)
+      // const data = nacl.encodeUTF8(utf8out);
+
+      this.setState({data: utf8out});
+    });
   }
 
   render() {
     return (
       <View>
-        <Text>{this.state.ohshit}</Text>
+        <Text>{this.state.data}</Text>
       </View>
     );
   }
